9ddde4a4884da2e2706719c7a6e3d91a2c325adc
kaliztro <g.lopes1999@hotmail.com>
2022-05-08 04:04:14 -0300

    melhorias para tornar sua experiencia ainda melhor

--- javascript/console.js
+++ javascript/console.js
@@ -1,1 +1,51 @@
-document.write(console.log(`EI!! O QUE VC ESTA FAZENDO AQUI?? SAIA LOGO OU EU IREI CHAMAR A MINHA MÃE!!`));
+console.log(`EI!! O QUE VC ESTA FAZENDO AQUI?? SAIA LOGO OU EU IREI CHAMAR A MINHA MÃE!!`);
+
+function add(){
+    location.href = "https://discord.com/api/oauth2/authorize?client_id=882715660134797342&permissions=0&scope=bot%20applications.commands";
+}
+
+function abrir(x){
+    if(x == 01) { document.getElementById('popup').style.display = 'block'; };
+    if(x == 02) { document.getElementById('popup02').style.display = 'block'; };
+    if(x == 03) { document.getElementById('popup03').style.display = 'block'; };
+    if(x == 04) { document.getElementById('popup04').style.display = 'block'; };
+    if(x == 05) { document.getElementById('popup05').style.display = 'block'; };
+    if(x == 06) { document.getElementById('popup06').style.display = 'block'; };
+    if(x == 07) { document.getElementById('popup07').style.display = 'block'; };
+    if(x == 08) { document.getElementById('popup08').style.display = 'block'; };
+    if(x == 09) { document.getElementById('popup09').style.display = 'block'; };
+    if(x == 10) { document.getElementById('popup10').style.display = 'block'; };
+    if(x == 11) { document.getElementById('popup11').style.display = 'block'; };
+    if(x == 12) { document.getElementById('popup12').style.display = 'block'; };
+    if(x == 13) { document.getElementById('popup13').style.display = 'block'; };
+    if(x == 14) { document.getElementById('popup14').style.display = 'block'; };
+    if(x == 15) { document.getElementById('popup15').style.display = 'block'; };
+    if(x == 16) { document.getElementById('popup16').style.display = 'block'; };
+    if(x == 19) { document.getElementById('popup19').style.display = 'block'; };
+    if(x == 21) { document.getElementById('popup21').style.display = 'block'; };
+    if(x == 22) { document.getElementById('popup22').style.display = 'block'; };
+    
+    
+}
+function fechar(x){
+    if(x == 01) { document.getElementById('popup').style.display =  'none'; };
+    if(x == 02) { document.getElementById('popup02').style.display =  'none'; };
+    if(x == 03) { document.getElementById('popup03').style.display =  'none'; };
+    if(x == 04) { document.getElementById('popup04').style.display =  'none'; };
+    if(x == 05) { document.getElementById('popup05').style.display =  'none'; };
+    if(x == 06) { document.getElementById('popup06').style.display =  'none'; };
+    if(x == 07) { document.getElementById('popup07').style.display =  'none'; };
+    if(x == 08) { document.getElementById('popup08').style.display =  'none'; };
+    if(x == 09) { document.getElementById('popup09').style.display =  'none'; };
+    if(x == 10) { document.getElementById('popup10').style.display =  'none'; };
+    if(x == 11) { document.getElementById('popup11').style.display =  'none'; };
+    if(x == 12) { document.getElementById('popup12').style.display =  'none'; };
+    if(x == 13) { document.getElementById('popup13').style.display =  'none'; };
+    if(x == 14) { document.getElementById('popup14').style.display =  'none'; };
+    if(x == 15) { document.getElementById('popup15').style.display =  'none'; };
+    if(x == 16) { document.getElementById('popup16').style.display =  'none'; };
+    if(x == 19) { document.getElementById('popup19').style.display =  'none'; };
+    if(x == 21) { document.getElementById('popup21').style.display =  'none'; };
+    if(x == 22) { document.getElementById('popup22').style.display =  'none'; };
+}
+
